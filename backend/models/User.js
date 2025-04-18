1ff2fef2c05fa3b9743586ba3c5984cb18887b7e
nhattVim <nhattruong13112000@gmail.com>
2025-04-18 21:21:02 +0700

    update something

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,22 @@ const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  phone: String,
+  token: String,
+  address: {
+    // Tỉnh
+    province: String,
+    // Thành phố,thị xã
+    district: String,
+    // Xã phường
+    ward: String,
+    // Đường
+    street: String
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
 }, { timestamps: true })
 
 // Hash password before saving
