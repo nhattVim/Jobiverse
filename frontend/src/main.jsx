748aed0e13eaca99012aa462b4d2b182bad9b06d
Luong Bao Phuc <ppbao1912@gmail.com>
2025-04-29 09:15:36 +0700

    update frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import Home from "./pages/home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
