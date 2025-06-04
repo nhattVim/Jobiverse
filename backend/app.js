7bbebd7606adacd55b81a6688b8bafbbd64f0984
nhattVim <nhattruong13112000@gmail.com>
2025-06-04 11:06:41 +0700

    feat: setup before deploy

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const logger = require('morgan')
 db.connect()
 
 // Middlewares
-app.use(cors({ origin: 'https://localhost:5173', credentials: true }))
+app.use(cors({ origin: true, credentials: true }))
 app.use(logger('dev'))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
