d413571bbbc69beeebffec3c6dd26675ea168cfe
nhattVim <nhattruong13112000@gmail.com>
2025-07-02 15:59:04 +0700

    update

--- backend.NET/Models/JobiverseContext.cs
+++ backend.NET/Models/JobiverseContext.cs
@@ -6,10 +6,6 @@ namespace api.Models;
 
 public partial class JobiverseContext : DbContext
 {
-    public JobiverseContext()
-    {
-    }
-
     public JobiverseContext(DbContextOptions<JobiverseContext> options)
         : base(options)
     {
@@ -58,10 +54,6 @@ public partial class JobiverseContext : DbContext
     public virtual DbSet<Student> Students { get; set; }
 
     public virtual DbSet<StudentsInterest> StudentsInterests { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=mysql-2a1c0cf2-nhattruong13112000-ee54.l.aivencloud.com;port=15833;database=Jobiverse;user=avnadmin;password=AVNS_-F7ERqsyjg4XaRBB0oh;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
